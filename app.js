312e60c711a9ebffe370824e45c8618ff3e5cdc9
jjmayasal <jjmayasal@gmail.com>
2025-03-11 06:22:53 -0500

    Se crea funcion agregaramigo

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo(){
+    let amigo = document.getElementById("amigo").value;
+    if (amigo.length > 0) {
+        amigos.push(amigo);
+        limpiarCaja("amigo")
+    }  else {
+        alert("Por favor, inserte un nombre")
+    }
+} 
+
+function limpiarCaja(id) {
+    document.querySelector(`#${id}`).value = '';
+}
