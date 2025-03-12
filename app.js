905dbcc768a20df219e4c64d55f277a2a221ad2f
jjmayasal <jjmayasal@gmail.com>
2025-03-12 06:50:16 -0500

    Crea la funcion agregarAmigo y sortearAmigo

--- app.js
+++ app.js
@@ -2,15 +2,40 @@
 let amigos = [];
 
 function agregarAmigo(){
-    let amigo = document.getElementById("amigo").value;
-    if (amigo.length > 0) {
-        amigos.push(amigo);
-        limpiarCaja("amigo")
+    let inputAmigo = document.getElementById("amigo");
+    let amigo = inputAmigo.value
+    if (!amigo) {
+        alert("Por favor, inserte un nombre");
     }  else {
-        alert("Por favor, inserte un nombre")
+        amigos.push(amigo);
+        inputAmigo.value = "";
+        inputAmigo.focus();
+        agregarAmigoHTML();
+        return
     }
 } 
 
-function limpiarCaja(id) {
-    document.querySelector(`#${id}`).value = '';
+// Una vez se agrega el nombre a la lista js "amigos" se debe mandar a la lista html
+function agregarAmigoHTML(){
+    let lista_html = document.getElementById("listaAmigos");
+    lista_html.innerHTML = "";
+
+    for (let amigo of amigos){
+        let nuevoAmigo = document.createElement("li");
+        nuevoAmigo.textContent = amigo
+        lista_html.appendChild(nuevoAmigo);
+    }
+    
+}
+
+//Ahora debemos sortear los amigos
+
+function sortearAmigo(){
+    if(amigos.length === 0){
+        alert("Aún no se puede sortear amigos");
+        return
+    }
+    let amigoSorteado = amigos[Math.floor(Math.random()*amigos.length)]
+    let resultado = document.getElementById("resultado")
+    resultado.innerHTML = `El amigo sorteado es ${amigoSorteado}`
 }
